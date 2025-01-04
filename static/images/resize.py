479291b2414f2b2249dc4de7c9bda57158c483d6
NAJIB <najib.azmi2019@gmail.com>
2025-01-04 19:20:58 +0100

    eee

--- static/images/resize.py
+++ static/images/resize.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 # Répertoire contenant les images
-image_dir = "static/images/sunglass"
+image_dir = "static/images/sweatshirt"
  # Remplacez par votre répertoire
 
 # Taille cible
