8a851db28cd3090cbab5d2a862ea47c3624279ed
NAJIB <najib.azmi2019@gmail.com>
2025-01-04 19:58:01 +0100

    eee

--- static/images/resize.py
+++ static/images/resize.py
@@ -2,34 +2,34 @@ import cv2
 import os
 
 # Répertoire contenant les images
-image_dir = "static/images/Windsor_tie"
- # Remplacez par votre répertoire
+root_dir = "static/images/"  # Remplacez par votre répertoire racine
 
 # Taille cible
-target_size = (260, 150)
+target_size = (260, 170)
 
-# Parcourir toutes les images dans le répertoire
-for filename in os.listdir(image_dir):
-    file_path = os.path.join(image_dir, filename)
-    
-    # Vérifier si le fichier est une image
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
-        try:
-            # Charger l'image
-            img = cv2.imread(file_path)
+# Parcourir tous les fichiers et dossiers dans le répertoire racine
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    for filename in filenames:
+        file_path = os.path.join(dirpath, filename)
 
-            # Vérifier si l'image est chargée correctement
-            if img is None:
-                print(f"Impossible de charger l'image : {file_path}")
-                continue
+        # Vérifier si le fichier est une image
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+            try:
+                # Charger l'image
+                img = cv2.imread(file_path)
 
-            # Redimensionner l'image
-            resized_img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
+                # Vérifier si l'image est chargée correctement
+                if img is None:
+                    print(f"Impossible de charger l'image : {file_path}")
+                    continue
 
-            # Écraser l'image originale avec l'image redimensionnée
-            cv2.imwrite(file_path, resized_img)
-            print(f"Image redimensionnée : {file_path}")
-        except Exception as e:
-            print(f"Erreur avec le fichier {filename}: {e}")
-    else:
-        print(f"Fichier ignoré (pas une image) : {filename}")
+                # Redimensionner l'image
+                resized_img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
+
+                # Écraser l'image originale avec l'image redimensionnée
+                cv2.imwrite(file_path, resized_img)
+                print(f"Image redimensionnée : {file_path}")
+            except Exception as e:
+                print(f"Erreur avec le fichier {file_path}: {e}")
+        else:
+            print(f"Fichier ignoré (pas une image) : {file_path}")
