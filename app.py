319f23611cb895f6b2718a33548075f4688257ed
NAJIB <najib.azmi2019@gmail.com>
2025-01-06 01:51:52 +0100

    ere

--- app.py
+++ app.py
@@ -21,16 +21,17 @@ def index():
     return render_template('index.html')
 
 # Route pour gérer le téléchargement d'image
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    search_query = request.form.get('search', '')  # Récupère la requête de recherche (si présente)
+    search_query = request.form.get('search', '')  # Récupère la requête de recherche
 
     if 'image' in request.files:
         file = request.files['image']
         if file.filename == '':
             return redirect(url_for('index'))
 
-        filename = secure_filename(file.filename)
+         = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
 
@@ -47,16 +48,24 @@ def upload():
 
         # Récupération des images similaires
         similar_images_folder = os.path.join('static/images', category)
-        if os.path.exists(similar_images_folder):
-            similar_images = os.listdir(similar_images_folder)
-        else:
-            similar_images = []
+        similar_images = os.listdir(similar_images_folder) if os.path.exists(similar_images_folder) else []
 
         # Filtrer les images en fonction de la recherche
         if search_query:
             similar_images = [img for img in similar_images if search_query.lower() in img.lower()]
 
         return render_template('index.html', uploaded_image=filename, category=category, similar_images=similar_images, search_query=search_query)
+
+    elif search_query:
+        # Gestion de la recherche textuelle seule
+        all_images_folder = 'static/images'
+        similar_images = []
+        for root, dirs, files in os.walk(all_images_folder):
+            for file in files:
+                if search_query.lower() in file.lower():
+                    similar_images.append(os.path.relpath(os.path.join(root, file), all_images_folder))
+        
+        return render_template('index.html', uploaded_image=None, similar_images=similar_images, search_query=search_query)
 
     return redirect(url_for('index'))
 
